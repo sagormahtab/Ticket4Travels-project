4963407abfaf2a4c7c77990de78119a47acbb7b7
Ezaz Ahmed <35024367+ezaz-ahmed@users.noreply.github.com>
2021-06-01 00:20:27 +0600

    Privacy Policy Changes

--- src/Components/Register_Log-in-page/RegisterPage-1/Privacy-Policy/PrivacyPolicy.js
+++ src/Components/Register_Log-in-page/RegisterPage-1/Privacy-Policy/PrivacyPolicy.js
@@ -1,167 +1,256 @@
-import { Card, CardContent } from "@material-ui/core";
-import React from "react";
-
 const PrivacyPolicy = () => {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-md-12">
-          <Card>
-            <CardContent>
-              <h3>Privacy Policy</h3>
-              <ul>
-                <li className="mt-2">
-                  Tickets4Travel built the Tickets4Travel app as a Commercial
-                  app. This SERVICE is provided by Tickets4Travel and is
-                  intended for use as is.
-                </li>
-                <li className="mt-5">
-                  This page is used to inform visitors regarding our policies
-                  with the collection, use, and disclosure of Personal
-                  Information if anyone decided to use our Service.
-                </li>
-                <li className="mt-5">
-                  If you choose to use our Service, then you agree to the
-                  collection and use of information in relation to this policy.
-                  The Personal Information that we collect is used for providing
-                  and improving the Service. We will not use or share your
-                  information with anyone except as described in this Privacy
-                  Policy.
-                </li>
-                <li className="mt-5">
-                  The terms used in this Privacy Policy have the same meanings
-                  as in our Terms and Conditions, which is accessible at
-                  Tickets4Travel unless otherwise defined in this Privacy
-                  Policy.
-                </li>
-              </ul>
-              <h3 className="mt-4">Information Collection and Use</h3>
-              <ul>
-                <li className="mt-2">
-                  For a better experience, while using our Service, we may
-                  require you to provide us with certain personally identifiable
-                  information. The information that we request will be retained
-                  by us and used as described in this privacy policy.
-                </li>
-                <li className="mt-5">
-                  The app does use third party services that may collect
-                  information used to identify you.
-                </li>
-                <li className="mt-5">
-                  Link to privacy policy of third party service providers used
-                  by the app
-                </li>
-              </ul>
+        <div className="col-md-12 ml-4 mr-4">
+          <h2 className="mt-3 mb-3">Privacy Policy for Tickets4Travel Ltd.</h2>
 
-              <h3 className="mt-4">Google Play Services Facebook Log Data</h3>
-              <ul>
-                <li className="mt-2">
-                  We want to inform you that whenever you use our Service, in a
-                  case of an error in the app we collect data and information
-                  (through third party products) on your phone called Log Data.
-                  This Log Data may include information such as your device
-                  Internet Protocol (“IP”) address, device name, operating
-                  system version, the configuration of the app when utilizing
-                  our Service, the time and date of your use of the Service, and
-                  other statistics.
-                </li>
-              </ul>
-              <h3 className="mt-4">Cookies</h3>
-              <ul>
-                <li className="mt-2">
-                  Cookies are files with a small amount of data that are
-                  commonly used as anonymous unique identifiers. These are sent
-                  to your browser from the websites that you visit and are
-                  stored on your device's internal memory.
-                </li>
-                <li className="mt-5">
-                  This Service does not use these “cookies” explicitly. However,
-                  the app may use third party code and libraries that use
-                  “cookies” to collect information and improve their services.
-                  You have the option to either accept or refuse these cookies
-                  and know when a cookie is being sent to your device. If you
-                  choose to refuse our cookies, you may not be able to use some
-                  portions of this Service.
-                </li>
-              </ul>
-              <h3 className="mt-4">Service Providers</h3>
-              <ul>
-                <li className="mt-2">
-                  We may employ third-party companies and individuals due to the
-                  following reasons:
-                </li>
-                <li className="mt-5">
-                  To facilitate our Service; To provide the Service on our
-                  behalf; To perform Service-related services; or To assist us
-                  in analyzing how our Service is used. We want to inform users
-                  of this Service that these third parties have access to your
-                  Personal Information. The reason is to perform the tasks
-                  assigned to them on our behalf. However, they are obligated
-                  not to disclose or use the information for any other purpose.
-                </li>
-              </ul>
-              <h3 className="mt-4">Security</h3>
-              <ul>
-                <li className="mt-2">
-                  We value your trust in providing us your Personal Information,
-                  thus we are striving to use commercially acceptable means of
-                  protecting it. But remember that no method of transmission
-                  over the internet, or method of electronic storage is 100%
-                  secure and reliable, and we cannot guarantee its absolute
-                  security.
-                </li>
-              </ul>
-              <h3 className="mt-4">Links to Other Sites</h3>
-              <ul>
-                <li className="mt-2">
-                  This Service may contain links to other sites. If you click on
-                  a third-party link, you will be directed to that site. Note
-                  that these external sites are not operated by us. Therefore,
-                  we strongly advise you to review the Privacy Policy of these
-                  websites. We have no control over and assume no responsibility
-                  for the content, privacy policies, or practices of any
-                  third-party sites or services.
-                </li>
-              </ul>
-              <h3 className="mt-4">Children’s Privacy</h3>
-              <ul>
-                <li className="mt-2">
-                  These Services do not address anyone under the age of 13. We
-                  do not knowingly collect personally identifiable information
-                  from children under 13. In the case we discover that a child
-                  under 13 has provided us with personal information, we
-                  immediately delete this from our servers. If you are a parent
-                  or guardian and you are aware that your child has provided us
-                  with personal information, please contact us so that we will
-                  be able to do necessary actions.
-                </li>
-              </ul>
-              <h3 className="mt-4">Changes to This Privacy Policy</h3>
-              <ul>
-                <li className="mt-2">
-                  We may update our Privacy Policy from time to time. Thus, you
-                  are advised to review this page periodically for any changes.
-                  We will notify you of any changes by posting the new Privacy
-                  Policy on this page.
-                </li>
-                <li className="mt-5">
-                  This policy is effective as of 2021-02-03
-                </li>
-              </ul>
-              <h3 className="mt-4">Contact Us</h3>
-              <ul>
-                <li className="mt-2">
-                  If you have any questions or suggestions about our Privacy
-                  Policy, do not hesitate to contact us at
-                  contact@tickets4travel.com.
-                </li>
-              </ul>
-            </CardContent>
-          </Card>
+          <p>
+            At Tickets4Travel, accessible from https://tickets4travel.com/, one
+            of our main priorities is the privacy of our visitors. This Privacy
+            Policy document contains types of information that is collected and
+            recorded by Tickets4Travel and how we use it.
+          </p>
+
+          <p>
+            If you have additional questions or require more information about
+            our Privacy Policy, do not hesitate to contact us.
+          </p>
+
+          <p>
+            This Privacy Policy applies only to our online activities and is
+            valid for visitors to our website with regards to the information
+            that they shared and/or collect in Tickets4Travel. This policy is
+            not applicable to any information collected offline or via channels
+            other than this website. Consent
+          </p>
+
+          <p>
+            By using our website, you hereby consent to our Privacy Policy and
+            agree to its terms.
+          </p>
+
+          <h3>Information we collect</h3>
+
+          <p>
+            The personal information that you are asked to provide, and the
+            reasons why you are asked to provide it, will be made clear to you
+            at the point we ask you to provide your personal information.
+          </p>
+
+          <p>
+            If you contact us directly, we may receive additional information
+            about you such as your name, email address, phone number, the
+            contents of the message and/or attachments you may send us, and any
+            other information you may choose to provide.
+          </p>
+
+          <p>
+            When you register for an Account, we may ask for your contact
+            information, including items such as name, company name, address,
+            email address, and telephone number.
+          </p>
+
+          <h3>How we use your information</h3>
+
+          <p>
+            We use the information we collect in various ways, including to:
+          </p>
+
+          <p>
+            Provide, operate, and maintain our website Improve, personalize, and
+            expand our website
+          </p>
+
+          <h3>Understand and analyze how you use our website</h3>
+
+          <p>
+            Develop new products, services, features, and functionality
+            Communicate with you, either directly or through one of our
+            partners, including for customer service, to provide you with
+            updates and other information relating to the website, and for
+            marketing and promotional purposes Send you emails Find and prevent
+            fraud
+          </p>
+
+          <h3>Log Files</h3>
+
+          <p>
+            Tickets4Travel follows a standard procedure of using log files.
+            These files log visitors when they visit websites. All hosting
+            companies do this and a part of hosting services' analytics. The
+            information collected by log files include internet protocol (IP)
+            addresses, browser type, Internet Service Provider (ISP), date and
+            time stamp, referring/exit pages, and possibly the number of clicks.
+            These are not linked to any information that is personally
+            identifiable. The purpose of the information is for analyzing
+            trends, administering the site, tracking users' movement on the
+            website, and gathering demographic information.
+          </p>
+
+          <h3>Cookies and Web Beacons</h3>
+
+          <p>
+            Like any other website, Tickets4Travel uses 'cookies'. These cookies
+            are used to store information including visitors' preferences, and
+            the pages on the website that the visitor accessed or visited. The
+            information is used to optimize the users' experience by customizing
+            our web page content based on visitors' browser type and/or other
+            information.
+          </p>
+
+          <p>
+            For more general information on cookies, please read "What Are
+            Cookies" from Cookie Consent.
+          </p>
+
+          <h3>Google DoubleClick DART Cookie</h3>
+
+          <p>
+            Google is one of a third-party vendor on our site. It also uses
+            cookies, known as DART cookies, to serve ads to our site visitors
+            based upon their visit to www.website.com and other sites on the
+            internet. However, visitors may choose to decline the use of DART
+            cookies by visiting the Google ad and content network Privacy Policy
+            at the following URL – https://policies.google.com/technologies/ads
+          </p>
+
+          <h3>Our Advertising Partners</h3>
+
+          <p>
+            Some of advertisers on our site may use cookies and web beacons. Our
+            advertising partners are listed below. Each of our advertising
+            partners has their own Privacy Policy for their policies on user
+            data. For easier access, we hyperlinked to their Privacy Policies
+            below.
+          </p>
+
+          <h3>Google</h3>
+
+          <p>https://policies.google.com/technologies/ads</p>
+
+          <h3>Advertising Partners Privacy Policies</h3>
+          <p>
+            You may consult this list to find the Privacy Policy for each of the
+            advertising partners of Tickets4Travel.
+          </p>
+
+          <p>
+            Third-party ad servers or ad networks uses technologies like
+            cookies, JavaScript, or Web Beacons that are used in their
+            respective advertisements and links that appear on Tickets4Travel,
+            which are sent directly to users' browser. They automatically
+            receive your IP address when this occurs. These technologies are
+            used to measure the effectiveness of their advertising campaigns
+            and/or to personalize the advertising content that you see on
+            websites that you visit.
+          </p>
+
+          <p>
+            Note that Tickets4Travel has no access to or control over these
+            cookies that are used by third-party advertisers.
+          </p>
+
+          <h3>Third Party Privacy Policies</h3>
+
+          <p>
+            Tickets4Travel's Privacy Policy does not apply to other advertisers
+            or websites. Thus, we are advising you to consult the respective
+            Privacy Policies of these third-party ad servers for more detailed
+            information. It may include their practices and instructions about
+            how to opt-out of certain options.
+          </p>
+
+          <p>
+            You can choose to disable cookies through your individual browser
+            options. To know more detailed information about cookie management
+            with specific web browsers, it can be found at the browsers'
+            respective websites.
+          </p>
+
+          <p>
+            CCPA Privacy Rights (Do Not Sell My Personal Information) Under the
+            CCPA, among other rights, California consumers have the right to:
+            Request that a business that collects a consumer's personal data
+            disclose the categories and specific pieces of personal data that a
+            business has collected about consumers.
+          </p>
+
+          <p>
+            Request that a business delete any personal data about the consumer
+            that a business has collected.
+          </p>
+
+          <p>
+            Request that a business that sells a consumer's personal data, not
+            sell the consumer's personal data.
+          </p>
+
+          <p>
+            If you make a request, we have one month to respond to you. If you
+            would like to exercise any of these rights, please contact us.
+          </p>
+
+          <h3>GDPR Data Protection Rights</h3>
+
+          <p>
+            We would like to make sure you are fully aware of all of your data
+            protection rights. Every user is entitled to the following:
+          </p>
+
+          <p>
+            The right to access – You have the right to request copies of your
+            personal data. We may charge you a small fee for this service.
+          </p>
+
+          <p>
+            The right to rectification – You have the right to request that we
+            correct any information you believe is inaccurate. You also have the
+            right to request that we complete the information you believe is
+            incomplete.
+          </p>
+
+          <p>
+            The right to erasure – You have the right to request that we erase
+            your personal data, under certain conditions.
+          </p>
+          <p>
+            The right to restrict processing – You have the right to request
+            that we restrict the processing of your personal data, under certain
+            conditions.
+          </p>
+          <p>
+            The right to object to processing – You have the right to object to
+            our processing of your personal data, under certain conditions.
+          </p>
+          <p>
+            The right to data portability – You have the right to request that
+            we transfer the data that we have collected to another organization,
+            or directly to you, under certain conditions.
+          </p>
+          <p>
+            If you make a request, we have one month to respond to you. If you
+            would like to exercise any of these rights, please contact us.
+          </p>
+          <h3>Children's Information</h3>
+
+          <p>
+            Another part of our priority is adding protection for children while
+            using the internet. We encourage parents and guardians to observe,
+            participate in, and/or monitor and guide their online activity.
+          </p>
+
+          <p>
+            Tickets4Travel does not knowingly collect any Personal Identifiable
+            Information from children under the age of 13. If you think that
+            your child provided this kind of information on our website, we
+            strongly encourage you to contact us immediately and we will do our
+            best efforts to promptly remove such information from our records.
+          </p>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default PrivacyPolicy;
+export default PrivacyPolicy
