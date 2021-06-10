48bb46f1f5b6f3e7bfe7a75b99802efe8b0eed3c
ezaz-ahmed <ezaznahmed@gmail.com>
2021-06-11 01:56:43 +0600

    added refund and ssl

---
+++ src/Components/Register_Log-in-page/RegisterPage-1/Refund-Policy/Refund.js
@@ -0,0 +1,147 @@
+import React from 'react'
+
+const Refund = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <h2>Return and Refund Policy </h2>
+          <p>Last updated: June 08, 2021 </p>
+          <p>Thank you for shopping at Tickets4Travel. </p>
+          <p>
+            If, for any reason, You are not completely satisfied with a purchase
+            We invite You to review our policy on refunds and returns.
+          </p>
+          <p>
+            The following terms are applicable for any products that You
+            purchased with Us. Interpretation and Definitions
+          </p>
+          <p>Interpretation</p>
+          <p>
+            The words of which the initial letter is capitalized have meanings
+            defined under the following conditions. The following definitions
+            shall have the same meaning regardless of whether they appear in
+            singular or in plural.
+          </p>
+          <h4>Definitions</h4>
+          <p>For the purposes of this Return and Refund Policy: </p>
+          <p>
+            Company (referred to as either "the Company", "We", "Us" or "Our" in
+            this Agreement) refers to Tickets4Travel, Ka-39/5, 4th floor C-5
+            KURIL Pragati Sarani Vatara, Dhaka, 1229.
+          </p>
+          <p>Services refer to the items offered for sale on the Service. </p>
+          <p>
+            Website refers to Tickets4Travel, accessible from
+            https://tickets4travel.com/
+          </p>
+          <p>
+            You mean the individual accessing or using the Service, or the
+            company, or other legal entity on behalf of which such individual is
+            accessing or using the Service, as applicable.
+          </p>
+          <h4>Your Order Cancellation Rights </h4>
+          <p>
+            You are entitled to cancel Your Order within 14 days without giving
+            any reason for doing so.
+          </p>
+          <p>
+            The deadline for cancelling an Order is 14 days from the date on
+            which You received the Services or on which a third party you have
+            appointed, who is not the carrier, takes possession of the product
+            delivered.{' '}
+          </p>
+          <p>
+            In order to exercise Your right of cancellation, You must inform Us
+            of your decision by means of a clear statement. You can inform us of
+            your decision by:{' '}
+          </p>
+          <p>By email: contact@tickets4travel.com </p>
+          <p>By phone number: 09613-123365 </p>
+          <p>
+            We will reimburse You no later than 14 days from the day on which We
+            receive the returned Services. We will use the same means of payment
+            as You used for the Order, and You will not incur any fees for such
+            reimbursement.{' '}
+          </p>
+          <h4>Conditions for Returns </h4>
+          <p>
+            In order for the Services to be eligible for a return, please make
+            sure that:
+          </p>
+          <p>The Services were purchased in the last 14 days</p>
+          <p>The Services are in the original packaging</p>
+          <p>The following Services cannot be returned:</p>
+          <p>
+            The supply of Services made to Your specifications or clearly
+            personalized.
+          </p>
+          <p>
+            The supply of Services which according to their nature are not
+            suitable to be returned, deteriorate rapidly or where the date of
+            expiry is over.
+          </p>
+          <p>
+            The supply of Services which are not suitable for return due to
+            health protection or hygiene reasons and were unsealed after
+            delivery.
+          </p>
+          <p>
+            The supply of Services which are, after delivery, according to their
+            nature, inseparably mixed with other items.
+          </p>
+          <p>
+            We reserve the right to refuse returns of any merchandise that does
+            not meet the above return conditions in our sole discretion.
+          </p>
+          <p>
+            Only regular priced Services may be refunded. Unfortunately,
+            Services on sale cannot be refunded. This exclusion may not apply to
+            You if it is not permitted by applicable law.
+          </p>
+
+          <h4>Returning Services </h4>
+          <p>
+            You are responsible for the cost and risk of returning the Services
+            to Us. You should send the Services at the following address:
+          </p>
+
+          <p>Ka-39/5, 4th floor C-5 KURIL Pragati Sarani Vatara, Dhaka, 1229</p>
+
+          <p>
+            We cannot be held responsible for Services damaged or lost in return
+            shipment. Therefore, We recommend an insured and trackable mail
+            service. We are unable to issue a refund without actual receipt of
+            the Services or proof of received return delivery.
+          </p>
+
+          <h4>Gifts</h4>
+
+          <p>
+            If the Services were marked as a gift when purchased and then
+            shipped directly to you, You'll receive a gift credit for the value
+            of your return. Once the returned product is received, a gift
+            certificate will be mailed to You.
+          </p>
+
+          <p>
+            If the Services weren't marked as a gift when purchased, or the gift
+            giver had the Order shipped to themselves to give it to You later,
+            We will send the refund to the gift giver.
+          </p>
+          <h4>Contact Us</h4>
+
+          <p>
+            If you have any questions about our Returns and Refunds Policy,
+            please contact us:
+          </p>
+
+          <p>By email: contact@tickets4travel.com</p>
+          <p>By phone number: 09613-123365</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Refund
