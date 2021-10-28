74ce2ff1173e57347b7ad4108ad1d59334941ef2
theshekraguy <learnuntilyouyo@gmail.com>
2021-10-28 13:37:41 +0600

    verify email page added

--- src/App.js
+++ src/App.js
@@ -1,50 +1,51 @@
-import React from 'react'
-import '../src/index.css'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import HeadFirst from './Components/Header-Of-Landing-Page/HeaderFirst'
-import Navbar from './Components/NavBar/Navbar'
-import Home from './Components/NavBar/pages/Home'
-import MyBooking from './Components/NavBar/pages/MyBooking'
-import MyInbox from './Components/NavBar/pages/MyInbox'
-import Saved from './Components/NavBar/pages/Saved'
-import HelpCenter from './Components/NavBar/pages/HelpCenter'
-import PartnerUs from './Components/NavBar/pages/PartnerUs'
-import ContactUs from './Components/NavBar/pages/ContactUs'
-import Footer from './Components/Footer_part/Footer'
-import Form2 from './Components/Register_Log-in-page/Account_Information/Form2'
-import FlightSearchPagesCombines from './Components/Flight_Search_Pages/FlightSearchPagesCombines'
-import FlightName from './Components/Flight_Search_Pages/Initial-Flight-booking-page/card1/FlightName'
-import BusSearchPagesCombines from './Components/Bus_Search_Pages/BusSearchPagesCombines'
-import BusPreBookingPageInfo from './Components/Bus_Search_Pages/Initial-Bus-booking-page/BusPreBookingPageInfo'
-import LaunchSearchPagesCombines from './Components/Launch_Search_Pages/LaunchSearchPagesCombines'
-import LaunchPreBookingPageInfo from './Components/Launch_Search_Pages/Initial-Launch-booking-page/LaunchPreBookingPageInfo'
-import HotelSearchPagesCombines from './Components/Hotel_Search_Pages/HotelSearchPagesCombines'
-import HotelPreBookingPageInfo from './Components/Hotel_Search_Pages/Initial-Hotel-booking-page/HotelPreBookingPageInfo'
-import TrainSearchPagesCombines from './Components/Train_Search_Pages/TrainSearchPagesCombines'
-import TrainPreBookingPageInfo from './Components/Train_Search_Pages/Initial-Train-booking-page/TrainPreBookingPageInfo'
-import BookingSteper_Combine from './Components/Stepper-page(booking,payment,process,Eticket)/BookingSteper_Combine'
-import About from './Components/Footer_part/AboutUs/About'
-import HotelDetails from './Components/Hotel_Search_Pages/PerHotelDetails-page/HotelDetails'
-import BusRoute from './Components/Bus_Search_Pages/BusRoute-ForFooter/BusRoute'
-import LaunchRoute from './Components/Launch_Search_Pages/LaunchRoute-ForFooter/LaunchRoute'
-import TrainRoute from './Components/Train_Search_Pages/TrainRoute-ForFooter/TrainRoute'
-import FlightRoute from './Components/Flight_Search_Pages/FlightRoute-ForFooter/FlightRoute'
-import LoginPage from './Components/LogIn-page/LoginPage'
-import RegistrationPage from './Components/Register_Log-in-page/RegistrationPage'
-import HotelSearch from './Components/Hotel_Search_Pages/HotelSearchFor-Footer/HotelSearch'
-import HowToBook from './Components/Footer_part/How-To-Book/HowToBook'
-import ContactWithUs from './Components/Footer_part/Contact-With-Us/ContactWithUs'
-import ScrollToTop from './Components/ScrollToTop'
-import PartnerShip from './Components/NavBar/PartnerShip/PartnerShip'
-import Terms from './Components/Register_Log-in-page/RegisterPage-1/Terms-and-Condition/Terms'
-import Refund from './Components/Register_Log-in-page/RegisterPage-1/Refund-Policy/Refund'
-import PrivacyPolicy from './Components/Register_Log-in-page/RegisterPage-1/Privacy-Policy/PrivacyPolicy'
-import ConfirmUserPage from './Components/Auth/ConfirmUserPage'
-import ForgotPassword from './Components/Auth/ForgotPassword'
-import ResetPassword from './Components/Auth/ResetPassword'
-import CareersWithUs from './Components/Footer_part/Careers-With-Us/CareersWithUs'
-import SubmitCv from './Components/Footer_part/Careers-With-Us/Submit-Your-CV/SubmitCv'
-import MeetWithUs from './Components/Footer_part/Meet-Our-Team/MeetWithUs'
+import React from "react";
+import "../src/index.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HeadFirst from "./Components/Header-Of-Landing-Page/HeaderFirst";
+import Navbar from "./Components/NavBar/Navbar";
+import Home from "./Components/NavBar/pages/Home";
+import MyBooking from "./Components/NavBar/pages/MyBooking";
+import MyInbox from "./Components/NavBar/pages/MyInbox";
+import Saved from "./Components/NavBar/pages/Saved";
+import HelpCenter from "./Components/NavBar/pages/HelpCenter";
+import PartnerUs from "./Components/NavBar/pages/PartnerUs";
+import ContactUs from "./Components/NavBar/pages/ContactUs";
+import Footer from "./Components/Footer_part/Footer";
+import Form2 from "./Components/Register_Log-in-page/Account_Information/Form2";
+import FlightSearchPagesCombines from "./Components/Flight_Search_Pages/FlightSearchPagesCombines";
+import FlightName from "./Components/Flight_Search_Pages/Initial-Flight-booking-page/card1/FlightName";
+import BusSearchPagesCombines from "./Components/Bus_Search_Pages/BusSearchPagesCombines";
+import BusPreBookingPageInfo from "./Components/Bus_Search_Pages/Initial-Bus-booking-page/BusPreBookingPageInfo";
+import LaunchSearchPagesCombines from "./Components/Launch_Search_Pages/LaunchSearchPagesCombines";
+import LaunchPreBookingPageInfo from "./Components/Launch_Search_Pages/Initial-Launch-booking-page/LaunchPreBookingPageInfo";
+import HotelSearchPagesCombines from "./Components/Hotel_Search_Pages/HotelSearchPagesCombines";
+import HotelPreBookingPageInfo from "./Components/Hotel_Search_Pages/Initial-Hotel-booking-page/HotelPreBookingPageInfo";
+import TrainSearchPagesCombines from "./Components/Train_Search_Pages/TrainSearchPagesCombines";
+import TrainPreBookingPageInfo from "./Components/Train_Search_Pages/Initial-Train-booking-page/TrainPreBookingPageInfo";
+import BookingSteper_Combine from "./Components/Stepper-page(booking,payment,process,Eticket)/BookingSteper_Combine";
+import About from "./Components/Footer_part/AboutUs/About";
+import HotelDetails from "./Components/Hotel_Search_Pages/PerHotelDetails-page/HotelDetails";
+import BusRoute from "./Components/Bus_Search_Pages/BusRoute-ForFooter/BusRoute";
+import LaunchRoute from "./Components/Launch_Search_Pages/LaunchRoute-ForFooter/LaunchRoute";
+import TrainRoute from "./Components/Train_Search_Pages/TrainRoute-ForFooter/TrainRoute";
+import FlightRoute from "./Components/Flight_Search_Pages/FlightRoute-ForFooter/FlightRoute";
+import LoginPage from "./Components/LogIn-page/LoginPage";
+import RegistrationPage from "./Components/Register_Log-in-page/RegistrationPage";
+import HotelSearch from "./Components/Hotel_Search_Pages/HotelSearchFor-Footer/HotelSearch";
+import HowToBook from "./Components/Footer_part/How-To-Book/HowToBook";
+import ContactWithUs from "./Components/Footer_part/Contact-With-Us/ContactWithUs";
+import ScrollToTop from "./Components/ScrollToTop";
+import PartnerShip from "./Components/NavBar/PartnerShip/PartnerShip";
+import Terms from "./Components/Register_Log-in-page/RegisterPage-1/Terms-and-Condition/Terms";
+import Refund from "./Components/Register_Log-in-page/RegisterPage-1/Refund-Policy/Refund";
+import PrivacyPolicy from "./Components/Register_Log-in-page/RegisterPage-1/Privacy-Policy/PrivacyPolicy";
+import VerifyEmailPage from "./Components/Auth/VerifyEmailPage";
+import ConfirmUserPage from "./Components/Auth/ConfirmUserPage";
+import ForgotPassword from "./Components/Auth/ForgotPassword";
+import ResetPassword from "./Components/Auth/ResetPassword";
+import CareersWithUs from "./Components/Footer_part/Careers-With-Us/CareersWithUs";
+import SubmitCv from "./Components/Footer_part/Careers-With-Us/Submit-Your-CV/SubmitCv";
+import MeetWithUs from "./Components/Footer_part/Meet-Our-Team/MeetWithUs";
 
 function App() {
   return (
@@ -162,6 +163,10 @@ function App() {
             component={BookingSteper_Combine}
           ></Route>
           <Route
+            path="/verify-email/:token"
+            component={VerifyEmailPage}
+          ></Route>
+          <Route
             path="/confirm-user/:token"
             component={ConfirmUserPage}
           ></Route>
@@ -174,7 +179,7 @@ function App() {
         <Footer />
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
